b812dc8322b849b627927491dcfea9ec96b1780c
is13126 <is13126@B206-2>
2023-01-23 12:07:43 +0700

    Добавьте файлы проекта.

---
+++ Катречко В.В ПЗ 4/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Катречко_В.В_ПЗ_4
+{
+    public class Times
+    {
+        public int year = 2021;
+
+    }
+    public class Time : Times
+    {
+        public int _hour;
+        public int _minute;
+        public int _second;
+        public Time(int hour, int minute, int second )
+        {
+            _hour = hour;
+            _minute = minute;
+            _second = second;
+        }
+
+        public void Read()
+        {
+            
+        }
+        public void Display()
+        {
+            Console.WriteLine($"$год {year} , часы {_hour}, минуты {_minute} ,second {_second}");
+        }
+    }
+}
---
+++ Катречко В.В ПЗ 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Катречко_В.В_ПЗ_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time t1 = new Time();
+            t1._hour = Convert.ToInt32(Console.ReadLine());
+            t1._minute = Convert.ToInt32(Console.ReadLine());
+            t1._second = Convert.ToInt32(Console.ReadLine());
+            Time t2 = new Time();
+            t2._hour = Convert.ToInt32(Console.ReadLine());
+            t2._minute = Convert.ToInt32(Console.ReadLine());
+            t2._second = Convert.ToInt32(Console.ReadLine());
+            Time t3 = new Time();
+            t3._hour = Convert.ToInt32(Console.ReadLine());
+            t3._minute = Convert.ToInt32(Console.ReadLine());
+            t3._second = Convert.ToInt32(Console.ReadLine());
+            t1.Display();
+            t2.Display();
+            t3.Display();
+        }
+    }
+}
